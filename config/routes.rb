0b1b4ffdd24f4f0b40244c9b95fd89ec78903f47
Patrice Drayton <draytonpatrice@marilynssecret.org>
2019-04-18 22:57:07 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-   resources :students, only: [:new, :create, :show, :edit, :update]
-  resources :school_classes, only: [:new, :create, :show, :edit, :update]
+  resources :students
+  resources :school_classes
+
+   
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
